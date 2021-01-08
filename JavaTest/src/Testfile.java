3a622bf59c80ffea5646c81763faa9d4f47b8e2f
byaganti <43803625+byaganti@users.noreply.github.com>
2021-01-08 10:25:20 +0530

    Update Testfile.java
    committing from github

--- JavaTest/src/Testfile.java
+++ JavaTest/src/Testfile.java
@@ -4,6 +4,7 @@ public class Testfile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("testing git checkin and checkout process");
+        System.out.println("checkin the file in git hub");
 	}
 
 }
