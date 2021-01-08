7c5ad3df3a38937e9bddb7e536beac6183e638b4
Raghunath <Raghunath@DESKTOP-NRCVKHQ>
2021-01-08 10:36:13 +0530

    new file added

---
+++ JavaTest/src/Testing.java
@@ -0,0 +1,11 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 10;
+    a = a + 15;
+    System.out.println("the value of a "+ a);
+	}
+
+}
